105d1cd632bd7b2dd0e4207c68953ac06a4aab04
Bruno Martins <bgcngm@gmail.com>
2018-08-01 16:28:27 +0200

    Daisy:  Build custom QTI telephony extension
    * This extension provides the ability to enable/disable SIM cards individually (the so-called manual SIM provisioning).
    
    * For real this time, using the open source implementation.
    
    Change-Id: Ia2ac619493c5cca6b068c3076e9d293612a219f7

--- device.mk
+++ device.mk
@@ -418,6 +418,7 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libcnefeatureconfig \
     libxml2 \
+    qti-telephony-common \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
