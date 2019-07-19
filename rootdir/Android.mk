163065fed96c60a53622c0028214d018c73a7a62
Offain <fabian.leutenegger@bluewin.ch>
2019-07-19 12:06:16 +0200

    daisy: Update FPC keylayout from stock
     * This fixes fingerprint gestures on FPC fingerprints
    Change-Id: I75cdc98a00f4080140fc8d12624af9eae48cbfed

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,17 +24,6 @@ LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
-# Offmode charging
-include $(CLEAR_VARS)
-LOCAL_MODULE          := chargeonlymode
-LOCAL_MODULE_OWNER    := xiaomi
-LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/chargeonlymode
-LOCAL_MODULE_TAGS     := optional
-LOCAL_MODULE_CLASS    := EXECUTABLES
-include $(BUILD_PREBUILT)
-
 # init.d
 include $(CLEAR_VARS)
 LOCAL_MODULE       := sysinit
