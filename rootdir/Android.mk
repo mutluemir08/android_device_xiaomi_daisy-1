7cd155cbd2ffcd72e78b1f2a9b53721d274d7218
acras01 <acras1@gmail.com>
2019-07-30 10:32:33 +0300

    Fix spectrum support

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,5 +40,5 @@ LOCAL_MODULE       := init.spectrum.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spectrum.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
