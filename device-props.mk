dd987b6a119fbe9778d4d9efa0b7f40810a98458
Tunahan <49642331+tunasahinn@users.noreply.github.com>
2019-08-21 01:17:18 +0300

    Added dex2oat and QTI Performance Modules

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,8 @@
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1
@@ -91,6 +96,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3=""
+
+# Perf
+ro.sys.fw.dex2oat_thread_count=8 \
+
+# QTI Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.enable_prefetch=1 \
+vendor.iop.enable_uxe=1 \
+vendor.iop.enable_prefetch_ofr=1 \
+vendor.perf.iop_v3.enable=1 \
+ro.vendor.at_library=libqti-at.so \
+persist.vendor.qti.games.gt.prof=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
